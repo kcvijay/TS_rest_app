c5657efee03363fbc214591e7a3f00c13bcd00f2
Vijay KC <vj.kc2007@gmail.com>
2023-03-01 11:14:31 +0200

    Get and post working

--- dist/app.js
+++ dist/app.js
@@ -5,5 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require("express"); ** Because TS does not have type support for app variable.
 const express_1 = __importDefault(require("express"));
+const todo_1 = __importDefault(require("./routes/todo"));
+const body_parser_1 = require("body-parser");
 const app = (0, express_1.default)();
+app.use((0, body_parser_1.json)());
 app.listen(3005);
+app.use("/todo", todo_1.default);
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message });
+});
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 // const express = require("express"); ** Because TS does not have type support for app variable.
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import todoRoutes from "./routes/todo";
+import { json } from "body-parser";
 
 const app = express();
+app.use(json());
 
 app.listen(3005);
+
+app.use("/todo", todoRoutes);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
